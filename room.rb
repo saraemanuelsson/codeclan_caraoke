be1b6c601b3be8efee2f0d9e184e604f1e743e2a
Sara Emanuelsson <sara256emanuelsson@gmail.com>
2020-05-22 18:22:06 +0100

    Add check-out guest and clear room function

--- room.rb
+++ room.rb
@@ -2,17 +2,29 @@ class Room
 
     attr_reader :name, :capacity, :songs, :guests, :song_queue, :bar_tab
 
-    def initialize(name, capacity)
+    def initialize(name, capacity, guests = [], songs)
         @name = name
         @capacity = capacity
         @songs = songs
-        @guests = []
+        @guests = guests
         @song_queue = []
         @bar_tab = 0
     end
 
+    def number_of_guests_in_room()
+        return @guests.size()
+    end
+
     def check_in_guest(guest)
         @guests.push(guest)
+    end
+
+    def check_out_guest(guest)
+        @guests.delete(guest)
+    end
+
+    def clear_room()
+        @guests.clear()
     end
 
 
--- specs/room_specs.rb
+++ specs/room_specs.rb
@@ -11,19 +11,21 @@ class TestRoom < MiniTest::Test
 
     def setup
 
-        @room = Room.new("Burn Baby Burn", 10)
+
 
         @guest1 = Guest.new("Bill", 3, "What does the fox say")
         @guest2 = Guest.new("Anna", 573, "I wanna dance with somebody")
         @guest3 = Guest.new("Linda", 47, "Whistle")
 
         @guests = [@guest1, @guest2, @guest3]
-
+        
         @song1 = Song.new("Don't stop believin'", "Journey")
         @song2 = Song.new("Livin' on a prayer", "Bon Jovi")
         @song3 = Song.new("Thrift shop", "Macklemore")
 
         @songs = [@song1, @song2, @song3]
+
+        @room = Room.new("Burn Baby Burn", 10, @guests, @songs)
 
     end
 
@@ -39,11 +41,18 @@ class TestRoom < MiniTest::Test
         @room.check_in_guest(@guest1)
         @room.check_in_guest(@guest2)
         @room.check_in_guest(@guest3)
-        assert_equal(3, @room.guests.size())
+        assert_equal(6, @room.number_of_guests_in_room())
     end
 
-    # def test_check_out_guests()
-    # end
+    def test_check_out_guest()
+        @room.check_out_guest(@guest2)
+        assert_equal(2, @room.number_of_guests_in_room())
+    end
+
+    def test_clear_room()
+        @room.clear_room()
+        assert_equal(0, @room.number_of_guests_in_room())
+    end
     
     # def test_add_songs_to_queue()
     # end
