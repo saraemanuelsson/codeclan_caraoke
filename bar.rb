5cce339504da3f07ce6f0fa65620c0e102c0ab3e
Sara Emanuelsson <sara256emanuelsson@gmail.com>
2020-05-23 16:04:17 +0100

    Add charge customer method

--- bar.rb
+++ bar.rb
@@ -17,5 +17,31 @@ class Bar
         @till += amount
     end
 
+    # def find_empty_room()
+
+    #     found_rooms = @rooms.find_all { |room| room.number_of_guests_in_room() == 0 }
+    #     # found_rooms = []
+
+    #     # for room in @rooms
+    #     #     found_rooms.push(room) if room.number_of_guests_in_room() == 0
+    #     # end
+
+    # end
+
+    def charge_one_customer(room, guest)
+        amount = room.bar_tab()
+
+        if guest.money_in_wallet > amount
+            guest.pay_bill(amount)
+            add_money_to_till(amount)
+        else
+            return "Call the police!"
+        end
+    end
+    
+    # def split_bill(room)
+
+    # end
+
 
 end
--- specs/bar_specs.rb
+++ specs/bar_specs.rb
@@ -2,7 +2,7 @@ require('minitest/autorun')
 require('minitest/reporters')
 require_relative('../bar')
 require_relative('../room')
-# require_relative('../guest')
+require_relative('../guest')
 
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
@@ -10,12 +10,16 @@ class TestBar < MiniTest::Test
 
     def setup
 
-    @songs = [@song1, @song2, @song3]
+        @songs = [@song1, @song2, @song3]
 
-    @room1 = Room.new("The Disco Room", 5, @songs, 30)
-    @room2 = Room.new("The Jazz Room", 15, @songs, 70)
+        @room1 = Room.new("The Disco Room", 5, @songs, 30)
+        @room2 = Room.new("The Jazz Room", 15, @songs, 70)
 
-    @bar = Bar.new("CodeClan Caraoke", @rooms)
+        @bar = Bar.new("CodeClan Caraoke", @rooms)
+
+        @guest1 = Guest.new("Bill", 3, "What does the fox say")
+        @guest2 = Guest.new("Anna", 573, "I wanna dance with somebody")
+        @guest3 = Guest.new("Linda", 47, "Whistle")
 
     end
 
@@ -31,5 +35,39 @@ class TestBar < MiniTest::Test
         @bar.add_money_to_till(10)
         assert_equal(10, @bar.total_money_in_till())
     end
+
+    # def test_find_empty_room()
+    #     @room2.check_in_guest(@guest1)
+    #     assert_equal([@room1], @bar.find_empty_room())
+    # end
+
+    # def test_add_customer_to_line_if_no_empty_room()
+    # end
+
+    # def test_add_customer_to_front_of_line_if_no_space_in_room()
+    # end
+
+    def test_charge_one_customer__enough_money()
+        @room2.increase_timer(110)
+        amount = @room2.charge_per_booking()
+        @room2.increase_bar_tab(amount)
+        @bar.charge_one_customer(@room2, @guest2)
+        assert_equal(433, @guest2.money_in_wallet())
+        assert_equal(140, @bar.total_money_in_till())
+    end
+
+    def test_charge_one_customer__not_enough_money()
+        @room2.increase_timer(110)
+        amount = @room2.charge_per_booking()
+        @room2.increase_bar_tab(amount)
+        result = @bar.charge_one_customer(@room2, @guest1)
+        assert_equal("Call the police!", result)
+        assert_equal(47, @guest3.money_in_wallet())
+        assert_equal(0, @bar.total_money_in_till())
+    end
+
+    # def test_split_bill()
+    #     assert_equal()
+    # end
 
 end
